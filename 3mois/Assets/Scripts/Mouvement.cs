be1941790b2d5df6d54390c2119f799b9336af58
clément bostyn <clemclem20002@gmail.com>
2023-09-26 11:34:52 +0200

    début player input

---
+++ 3mois/Assets/Scripts/Mouvement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class Mouvement : MonoBehaviour
+{
+    [BoxGroup("Stats Player")]
+    [SerializeField]
+    private float mouveSpeed;
+
+}
